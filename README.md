73539bbb036feed76430081a7f3a5c3dfbcab0d0
gcarvellas <gcarvel264@gmail.com>
2023-05-07 16:03:56 -0400

    init

---
+++ README.md
@@ -0,0 +1,20 @@
+# Dotfiles
+
+## Requirements
+
+- i3 v4.22
+- dunst v1.9.0
+- brillo v1.4.12 https://gitlab.com/cameronnemo/brillo
+- Font Awesome 6
+- powerprofilesctl 0.12
+- pactl 16.1
+- gnome terminal 3.48.0
+
+## Images
+![image](./images/1.png)
+![image](./images/2.png)
+
+## References
+
+- https://gitlab.com/Nmoleo/i3-volume-brightness-indicator
+- https://github.com/altindas/dots
---
+++ bin/battery_mode.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Toggles between power saving modes. Used for i3wm
+
+mode=$(powerprofilesctl get)
+
+case $mode in
+
+#toggle from power-saver to performance
+power-saver)
+	powerprofilesctl set performance
+	dunstify -u normal -t 1000 -r 2593 "Set Power Mode to Performance"
+	;;
+
+#toggle from performance to balanced
+performance)
+	powerprofilesctl set balanced
+	dunstify -u normal -t 1000 -r 2593 "Set Power Mode to Balanced"
+	;;
+
+#toggle from balanced to power-saver
+balanced)
+	powerprofilesctl set power-saver
+	dunstify -u normal -t 1000 -r 2593 "Set Power Mode to Power Saver"
+	;;
+esac
