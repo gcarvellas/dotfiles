604adeac4444db294a7676f8c70f93887e776681
gcarvellas <gcarvel264@gmail.com>
2023-07-06 22:00:45 -0400

    updated monitor refresh

--- bin/monitor_refresh.sh
+++ bin/monitor_refresh.sh
@@ -3,7 +3,8 @@
 if [[ "$(xrandr --listmonitors | wc -l)" -le "3" ]]; then
 	xrandr --output eDP1 --mode 1920x1080 --rate 144.00 --primary;
 else
-	# Hometown Setup. 3 Monitors with laptop in the middle.
+	# Hometown Setup
+	xrandr --output eDP1 --off;
 	xrandr --output DP-4 --primary --rate 144.00 --mode 1920x1080 --output eDP1 --mode 1920x1080 --rate 144.00 --right-of DP-4 --output HDMI-0 --mode 1600x900 --rate 60.00 --right-of eDP1;
 fi
 
